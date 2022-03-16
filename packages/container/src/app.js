56f99aecaa8f2c26fb319ccc84b5c63cd47f28e6
Ransome <ransome.coleman@gmail.com>
2022-03-16 16:08:41 -0400

    Updating to trigger workflow

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
   return <div>
-    <h1>Hellow world 2!</h1>
+    <h1>Hellow world!</h1>
     <hr />
     <MarketingApp />
   </div>
